489c85e6c8b0db6631bad699592493fef0b77178
Vasek <vasek.dev@gmail.com>
2023-06-08 21:47:05 -0300

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
     ? '/foobar1/'
-    : '/'
-}
+    : '/',
+  transpileDependencies: true
+})
